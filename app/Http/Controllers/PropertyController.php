ccd1dceac776412c696ee35d55176f5703beea85
Miller Adulu <milleradulu@gmail.com>
2018-04-24 11:11:41 +0300

    Fixed search by minPrice and maxPrice

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -30,70 +30,73 @@ class PropertyController extends Controller
 	return view('property.create',compact('types'));
   }
 
-  public function search(){
+  public function search()
+  {
 	$types = Type::all();
 	return view('property.search', compact('types'));
   }
 
-  public function searchby(Request $request) {
-    $parameter = $request['parameter'];
-    $category = $request['category'];
-    
-    $properties = null;
+  public function searchby(Request $request)
+  {
+	$parameter = $request['parameter'];
+	$category = $request['category'];
 
-    if($category == 0):
+	$properties = null;
+
+	if($category == 0):
 	  $properties = $this->searchByLocation($parameter);
-    elseif ($category == 1):
+	elseif ($category == 1):
+	  $properties = $this->searchByMinPrice($parameter);
+	elseif ($category == 2):
 	  $properties = $this->searchByMaxPrice($parameter);
-    elseif ($category == 2):
-    $properties = $this->searchByMinPrice($parameter);
-    elseif ($category == 3):
-    $properties = $this->searchByMode(0);
-    elseif ($category == 3):
-    $properties = $this->searchByMode(1);
+	elseif ($category == 3):
+	  $properties = $this->searchByMode(0);
+	elseif ($category == 4):
+	  $properties = $this->searchByMode(1);
 	endif;
 
 	return view('property.results', compact('properties'));
   }
 
-  public function searchByLocation($location) {
-	$properties = Property::where('location', $location)->get();
-	return $properties;
+  public function searchByLocation($location)
+  {
+	return Property::where('location', $location)->get();
   }
 
-  public function searchByMaxPrice($maxPrice) {
-    $properties = Property::where('price', '<=', $maxPrice)->get();
-    return $properties;
+  public function searchByMaxPrice($maxPrice)
+  {
+	return Property::where('price', '<=', $maxPrice)->get();
   }
 
-  public function searchByMinPrice($minPrice) {
-	$properties = Property::where('price', '>=', $minPrice)->get();
-	return $properties;
+  public function searchByMinPrice($minPrice)
+  {
+	return Property::where('price', '>=', $minPrice)->get();
   }
 
-  public function searchByBedrooms($bedrooms) {
-	$properties = Property::where('bedrooms', '=>', $bedrooms)->get();
-	return $properties;
+  public function searchByBedrooms($bedrooms)
+  {
+	return Property::where('bedrooms', '=>', $bedrooms)->get();
   }
 
-  public function searchByMode($mode) {
-	$properties = Property::where('mode', $mode)->get();
-	return $properties;
+  public function searchByMode($mode)
+  {
+	return Property::where('mode', $mode)->get();
   }
 
-  public function searchByBathrooms($bathrooms) {
-	$properties = Property::where('bathrooms', '=>', $bathrooms)->get();
-	return $properties;
+  public function searchByBathrooms($bathrooms)
+  {
+	return Property::where('bathrooms', '=>', $bathrooms)->get();
   }
 
-  public function searchBySize($size) {
-	$properties = Property::where('size', '=>', $size)->get();
-	return $properties;
+  public function searchBySize($size)
+  {
+	return Property::where('size', '=>', $size)->get();
   }
 
 
 
-//  public function searchResults(Request $request){
+//  public function searchResults(Request $request)
+//{
 //	$min_price = ['price', '=>', $request['price_min']];
 //	$max_price = ['price', '<=', $request['price_max']];
 //	$constraints = array_only(Request::all(), [
@@ -130,11 +133,9 @@ class PropertyController extends Controller
 	$property->description = $request['description'];
 	// $property->pic=$request['pic'];
 
-
 	$property->save();
 
 	return redirect()->back();
-
   }
 
   /**
--- resources/views/property/search.blade.php
+++ resources/views/property/search.blade.php
@@ -12,24 +12,25 @@
 ​
 
 <form action="/search" method="post">
-{{csrf_field()}}
 
-Property_name:<br>
+  {{csrf_field()}}
+
+  Property_name:<br>
 
   <input type="text" name="property_name" value="name">
 
   <br>
-Location:<br>
+  Location:<br>
 
   <input type="text" name="location">
 
   <br>
-Mode:<br>
+  Mode:<br>
   <input type="radio" name="mode" value="0"> For Sale<br>
   <input type="radio" name="mode" value="1"> For Rent<br>
   <input type="radio" name="mode" value="2"> Open Status<br>
   <br>
-Price Min:<br>
+  Price Min:<br>
 
   <input type="text" name="price_min" >
 
@@ -39,64 +40,64 @@ Price Min:<br>
 
   <br>
   <br>
-Bedrooms:<br>
+  Bedrooms:<br>
 
-<select name="beds">
-  <option value="1">1+ bed</option>
-  <option value="2">2+ bed</option>
-  <option value="3">3+ bed</option>
-  <option value="4">4+ bed</option>
-  <option value="5">5+ bed</option>
-</select>
-<br>
-Bathrooms:<br>
-<select name="baths">
-<option value="1">1+ bats</option>
-  <option value="2">2+ bats</option>
-  <option value="3">3+ bats</option>
-  <option value="4">4+ bats</option>
-  <option value="5">5+ bats</option>
-</select>
-
-<br>
-<br>
-Property_type:<br>
-
- <select name="type">
- @foreach($types as $type)
- <option value="{{ $type->id }}">{{ $type->type }}</option>
- @endforeach
-
-</select> 
+  <select name="beds">
+    <option value="1">1+ bed</option>
+    <option value="2">2+ bed</option>
+    <option value="3">3+ bed</option>
+    <option value="4">4+ bed</option>
+    <option value="5">5+ bed</option>
+  </select>
+  <br>
+  Bathrooms:<br>
+  <select name="baths">
+    <option value="1">1+ bats</option>
+    <option value="2">2+ bats</option>
+    <option value="3">3+ bats</option>
+    <option value="4">4+ bats</option>
+    <option value="5">5+ bats</option>
+  </select>
 
   <br>
-Square Feet:<br>
-<select>
-  <option value="sq">250+ sqft</option>
-  <option value="sq">500+ sqft</option>
-  <option value="sq">1000+ sqft</option>
-  <option value="sq">1500+ sqft</option>
-  <option value="sq">2000+ sqft</option>
-  <option value="sq">2500+ sqft</option>
-  <option value="sq">3000+ sqft</option>
-  <option value="sq">3500+ sqft</option>
-  <option value="sq">4000+ sqft</option>
-  <option value="sq">4500+ sqft</option>
-  <option value="sq">5000+ sqft</option>
-</select>
+  <br>
+  Property_type:<br>
 
-  
+  <select name="type">
+    @foreach($types as $type)
+      <option value="{{ $type->id }}">{{ $type->type }}</option>
+    @endforeach
+
+  </select>
+
+  <br>
+  Square Feet:<br>
+  <select>
+    <option value="sq">250+ sqft</option>
+    <option value="sq">500+ sqft</option>
+    <option value="sq">1000+ sqft</option>
+    <option value="sq">1500+ sqft</option>
+    <option value="sq">2000+ sqft</option>
+    <option value="sq">2500+ sqft</option>
+    <option value="sq">3000+ sqft</option>
+    <option value="sq">3500+ sqft</option>
+    <option value="sq">4000+ sqft</option>
+    <option value="sq">4500+ sqft</option>
+    <option value="sq">5000+ sqft</option>
+  </select>
+
+
 
   <br>
 
-<!-- Featured_image:
-<input type="file" name="pic" accept="image/*"> -->
+  <!-- Featured_image:
+	<input type="file" name="pic" accept="image/*"> -->
   <!-- <input type="submit"> -->
 
 
   <input type="submit" value="Submit">
 
-</form> 
+</form>
 
 ​
 ​
