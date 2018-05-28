df1c8e971619cd5c9c2f0c9a45dc5bc531c86835
Miller Adulu <milleradulu@gmail.com>
2018-05-28 09:59:51 +0300

    Comments for blogs and properties

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -57,7 +57,32 @@
 			</div>
 
 		</div>
+
 	</div>
 
 	@include('chat.chat')
+
+	<!-- Go to www.addthis.com/dashboard to customize your tools -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b0298288d5ff3b6"></script>
+	<!-- Disqus -->
+	<div id="disqus_thread" style="margin-left: 5%; margin-right:5%;"></div>
+	<script>
+
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        var disqus_config = function () {
+            this.page.url = '{{ Request::url() }}';  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = '{{ $post->slug }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://kensim.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', + new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	<script id="dsq-count-scr" src="//kensim.disqus.com/count.js" async></script>
+
 @endsection
